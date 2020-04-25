2099ad2e1108fb59d17e5d5a4fae7dd48ecfd56a
YeEeck <64213918+YeEeck@users.noreply.github.com>
2020-04-26 00:54:53 +0800

    Add files via upload

--- src/accurate.c
+++ src/accurate.c
@@ -21,38 +21,50 @@ void char2spell(const char s[], char d[]){
 
 int main() {
     char buf[100], spell[100], path[100], buf2[100];
+    char head[99], last[99];
+    char list[99][10];
     FILE *fp;
     int i = 0, a;
+    printf("————精准匹配模式启动（只匹配前后字符相同的成语，拼音相同不再作为条件）————\n");
+    srand((unsigned)time(NULL));
     while (1){
-        srand((unsigned)time(NULL));
         i = 0;
         printf("请输入成语(四字)：\n");
         scanf("%s", buf);
         if (buf[0] == '#') break;
-        char2spell(buf, spell);
-        if (strlen(buf) != 8){
+        if (strlen(buf) != 8) {
             printf("你输入的不是四字成语哦。\n\n");
             continue;
         }
+        char2spell(buf, spell);
         sprintf(path, "list/%s.txt", spell);
-        if (access(path, F_OK) != 0){
-            printf("无法找到匹配的成语。\n");
-            continue;
-        }
+        strcpy(last, buf + 6);
+//        if (access(path, F_OK) != 0){
+//            printf("无法找到匹配的成语。\n");
+//            continue;
+//        }
 
         fp = fopen(path, "r");
         while(feof(fp) == 0){
             fscanf(fp, "%s", buf2);
-            i++;
+            sprintf(head, "%.2s", buf2);
+            if (strcmp(head, last) == 0){
+                strcpy(list[i], buf2);
+                i++;
+            }
         }
-
+        if (i == 0){
+            printf("无法找到匹配的成语。\n\n");
+            continue;
+        }
         a = rand()%i;
-        printf("找到%d个候选，选择第%d个。\n", i, a);
+        printf("找到%d个候选，选择第%d个。\n", i + 1, a + 1);
         rewind(fp);
-        for (int j = 0; j < a; ++j) {
-            fscanf(fp, "%s", buf2);
-        }
-        printf("接：%s\n\n", buf2);
+//        for (int j = 0; j < a; ++j) {
+//            fscanf(fp, "%s", buf2);
+//        }
+
+        printf("接：%s\n\n", list[a]);
         fclose(fp);
     }
     return 0;
